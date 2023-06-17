2be174fe722fe52cb4deeecebaefb3a0aa7c2697
tikogakhutishvili <tikogakhutishvili26@gmail.com>
2023-06-17 15:18:32 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let Sum = 0
+for(i=0; i<=10; i++){
+    Sum+=i
+}
+console.log(Sum/2)
+
+
+let char = "tiko"
+let Upper = char.toLocaleUpperCase()
+for(i=0;i<=char.length; i++){
+    console.log(Upper[i])
+}
